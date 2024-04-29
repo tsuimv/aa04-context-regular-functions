e98b8f09181c69c2ae1e6234079344d4f1432be3
Mitchell Tsui <tsuimv@DESKTOP-CR6MCST.localdomain>
2024-04-29 19:37:34 -0400

    final

--- context.js
+++ context.js
@@ -16,22 +16,23 @@ const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
 // eat(); // ?
-
+// undefined eats fish food
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
 // nemo.eat(); // ?
-
+// Nemo eats fish food
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
 // eat(); // ?
-
+// undefined eats fish food.
 
 /********************************* Scenario 4 *********************************/
 // nemo.swim(); // ?
-
+// Nemo swimming in the water
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
 // swim(); // ?
+// Error, 
